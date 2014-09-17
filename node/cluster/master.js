108f769da8dd9c80f8b82f90bf87d380c34df9a2
kevin <fanchangyong@gmail.com>
2014-09-17 10:23:31 +0800

    master

--- node/cluster/master.js
+++ node/cluster/master.js
@@ -1,24 +1,44 @@
 var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
+var conns = [];
 
-function fork(){
-	var worker = cluster.fork();
-	worker.on('message',function(msg){
-		if(msg=='crash'){
-			fork();
+function msgHandler(msg){
+	conns.push(msg.conn);
+	if(conns.length==numCPUs){
+		var sum = 0;
+		for(var j=0;j<conns.length;j++){
+			sum+=conns[j];
 		}
-	});
+		console.log('msg conn:',msg.conn);
+		conns=[];
+	}
 }
 
 if (cluster.isMaster) {
-	for (var i = 0; i < numCPUs; i++) {
-		fork();
-	}
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    var worker = cluster.fork();
+		worker.on('message',msgHandler);
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+
 } else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+	
+	var conn = 0;
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+		conn++;
+  }).listen(8000);
+
 	setInterval(function(){
-		console.log('child crash...');
-		cluster.worker.send('crash');
-		process.exit(0);
-	},2000);
+		process.send({conn:conn});
+		conn = 0;
+	},1000);
 }
